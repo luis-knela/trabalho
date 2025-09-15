5c2286f0476e5cc5d4c0b531487baed374971cd2
luis-knela <luis.assis@estudante.ifms.edu.br>
2025-09-15 11:41:54 -0400

    codigo base

---
+++ scripts.js
@@ -0,0 +1,48 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const passwordconfirmation = document.getElementById('password-confirmation');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    checkinputs();
+})
+
+function checkinputs() {
+    const usernameValue = username.value;
+    const emailValue = email.value;
+    const passwordValue = password.value;
+    const passwordconfirmationValue = passwordconfirmation.value;
+
+
+    if (usernameValue === '') {
+        setErrorFor(username, 'campo obrigatorio');
+    } else {
+        setSuccessFor(username);
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+
+    small.innerText = message;
+
+    formControl.className = 'form-control error';
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+
+    formControl.className = 'form-control success';
+}
+
+
+
+function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@']+(\.[^<>()\[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email
+    );
+}
